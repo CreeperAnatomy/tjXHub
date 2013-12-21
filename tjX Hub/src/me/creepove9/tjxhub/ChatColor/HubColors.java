7879862dd1817c0bb252eca1443891d33f7bdfa9
apple <apple@10.0.0.72>
2013-12-21 22:02:15 +1000

    Added ChatColors

--- tjX Hub/src/me/creepove9/tjxhub/ChatColor/HubColors.java
+++ tjX Hub/src/me/creepove9/tjxhub/ChatColor/HubColors.java
@@ -5,11 +5,29 @@ import org.bukkit.ChatColor;
 public class HubColors //Yup
 {
 
+	
 	ChatColor a = ChatColor.AQUA;
 	ChatColor b = ChatColor.BLACK;
 	ChatColor c = ChatColor.BLUE;
 	ChatColor d = ChatColor.DARK_AQUA;
 	ChatColor e = ChatColor.DARK_BLUE;
-	
+	ChatColor f = ChatColor.DARK_GRAY;
+	ChatColor g = ChatColor.DARK_GREEN;
+	ChatColor h = ChatColor.DARK_PURPLE;
+	ChatColor i = ChatColor.DARK_RED;
+	ChatColor j = ChatColor.GOLD;
+	ChatColor k = ChatColor.GRAY;
+	ChatColor l = ChatColor.GREEN;
+	ChatColor m = ChatColor.LIGHT_PURPLE;
+	ChatColor n = ChatColor.MAGIC;
+	ChatColor o = ChatColor.RED;
+	ChatColor p = ChatColor.WHITE;
+	ChatColor q = ChatColor.YELLOW;
+	// Symbols
+	ChatColor r = ChatColor.RESET;
+	ChatColor bo = ChatColor.BOLD;
+	ChatColor it = ChatColor.ITALIC;
+	ChatColor st = ChatColor.STRIKETHROUGH;
+	ChatColor un = ChatColor.UNDERLINE;
 	
 }
--- tjX Hub/src/me/creepove9/tjxhub/command/CommandHandler.java
+++ tjX Hub/src/me/creepove9/tjxhub/command/CommandHandler.java
@@ -1,5 +1,7 @@
 package me.creepove9.tjxhub.command;
 
+import me.creepove9.tjxhub.ChatColor.HubColors;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -7,7 +9,33 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class CommandHandler implements CommandExecutor {
+		
+		ChatColor a = ChatColor.AQUA;
+		ChatColor b = ChatColor.BLACK;
+		ChatColor c = ChatColor.BLUE;
+		ChatColor d = ChatColor.DARK_AQUA;
+		ChatColor e = ChatColor.DARK_BLUE;
+		ChatColor f = ChatColor.DARK_GRAY;
+		ChatColor g = ChatColor.DARK_GREEN;
+		ChatColor h = ChatColor.DARK_PURPLE;
+		ChatColor i = ChatColor.DARK_RED;
+		ChatColor j = ChatColor.GOLD;
+		ChatColor k = ChatColor.GRAY;
+		ChatColor l = ChatColor.GREEN;
+		ChatColor m = ChatColor.LIGHT_PURPLE;
+		ChatColor n = ChatColor.MAGIC;
+		ChatColor o = ChatColor.RED;
+		ChatColor p = ChatColor.WHITE;
+		ChatColor q = ChatColor.YELLOW;
+		// Symbols
+		ChatColor r = ChatColor.RESET;
+		ChatColor bo = ChatColor.BOLD;
+		ChatColor it = ChatColor.ITALIC;
+		ChatColor st = ChatColor.STRIKETHROUGH;
+		ChatColor un = ChatColor.UNDERLINE;
+		
 
+	
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
@@ -21,12 +49,12 @@ public class CommandHandler implements CommandExecutor {
 				Player p = (Player) sender;
 				
 				if (args.length == 0) {
-					sender.sendMessage()
+					sender.sendMessage(a + "             [Help]");
 				}
 			}
 		}
 		
 		return false;
 	}
-
+	
 }
